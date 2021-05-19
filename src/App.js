a5547d23ffa0b5e797db936bceb550cc81fd4625
DaniPinelli <danielpinelli.fullstack@gmail.com>
2021-05-19 18:46:03 -0300

    Working on search engine

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@ import React, { Component} from 'react';
 import SearchEngine from './components/SerachEngine';
 
 class App extends Component {
+
+  state = {
+    words: ''
+  }
+
+  consultApi = () => {
+
+    const url = `https://pixabay.com/api/?key=21699074-abf7b4c7bfe9ac3ab0e58bbd7&q=${this.state.words}`;
+  }
+
+  dataSearched = (words) => {
+    this.setState({
+      words
+    }, () => {
+      this.consultApi();
+    })
+  }
+
   render() {
     return (
       <div className="app container">
         <div className="jumbotron" >
         <p className="lead text-center" >Image Search Engine</p>
-        <SearchEngine/>
+        <SearchEngine
+        dataSearched= {this.dataSearched}
+        />
         </div>
         
       </div>
--- src/components/SerachEngine.js
+++ src/components/SerachEngine.js
@@ -4,12 +4,20 @@ class SearchEngine extends Component{
 
     searchRef = React.createRef();
 
+    getData = (e) => {
+        e.preventDefault();
+
+        const words = this.searchRef.current.value;
+
+        this.props.dataSearched(words);
+    }
+
     render() {
         return ( 
-          <form>
+          <form onSubmit={this.getData} >
               <div className="row" >
                 <div className="form-group col-md-8 " >
-                    <input ref={this.searchRef} type="text" className="form-control form-control-lg" placeholder="Type a word, example: Cars" />
+                    <input ref={this.searchRef} type="text" className="form-control form-control-lg" placeholder="Type a word, for example: Cars" />
                 </div>
                 <div className="form-group col-md-4 " >
                     <input type="submit" className="btn btn-lg btn-success btn-block" value="Search" />
